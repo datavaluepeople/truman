0bac7913ecbbbdc183e10d8093a3439b526c4f92
dbcerigo <dbcerigo@gmail.com>
2020-07-26 12:48:06 +0100

    Implement generalised time-contextual bandits

--- sellsim/interface.py
+++ sellsim/interface.py
@@ -1,4 +1,4 @@
-from typing import Callable, Dict, List
+from typing import Callable, Dict, List, Protocol
 
 import numpy as np
 
@@ -41,17 +41,9 @@ class HeirarchicalStaticBernoulliBandits():
         return self.bandits[selected_bandit].action(multiplier=context_multiplier)
 
 
-class PseudoStaticBernoulliBandits():
-    def __init__(self, bandits: List[Bandit], periodicity: Callable[[int], float]):
-        self.bandits = bandits
-        self.periodicity = periodicity
-        self.timestep = 0
-
-    def step(self, selected_bandit: int) -> bool:
-        """Use selected_bandit to do bandit.action()"""
-        periodicity_multiplier = self.periodicity(self.timestep)
-        self.timestep += 1
-        return self.bandits[selected_bandit].action(multiplier=periodicity_multiplier)
+class StepperModifier(Protocol):
+    def step(self) -> float:
+        ...
 
 
 def eg_weekly_periodicity(timestep: int) -> float:
@@ -59,3 +51,49 @@ def eg_weekly_periodicity(timestep: int) -> float:
     weekly_periodicity = [1.0] * 5 + [1.1] * 2
     day_of_week = timestep % 7
     return float(weekly_periodicity[day_of_week])
+
+
+class Periodicity():
+    def __init__(self, periodicity: Callable[[int], float]):
+        self.timestep = 0
+        self.periodicity = periodicity
+
+    def step(self) -> float:
+        multiplier = self.periodicity(self.timestep)
+        self.timestep += 1
+        return multiplier
+
+
+class RandomWalkTrend():
+    def __init__(self, lower: float, upper: float, step_size: float):
+        self.modifier = 1.0
+        self.lower = lower
+        self.upper = upper
+        self.step_size = step_size
+
+    def step(self) -> float:
+        direction = +1.0 if np.random.random() < 0.5 else -1.0
+        self.modifier += direction * self.step_size
+        self.modifier
+        self.modifier = min(self.upper, self.modifier)
+        self.modifier = max(self.lower, self.modifier)
+        return self.modifier
+
+
+class TimestepContextualBernoulliBandits():
+    def __init__(self, bandits: List[Bandit], step_contexts: List[StepperModifier]):
+        self.bandits = bandits
+        self.step_contexts = step_contexts
+        self.timestep = 0
+
+    def step(self, selected_bandit: int) -> bool:
+        context_multiplier = 1.0
+        for context in self.step_contexts:
+            context_multiplier *= context.step()
+        self.timestep += 1
+
+        return self.bandits[selected_bandit].action(multiplier=context_multiplier)
+weekly_with_trend = TimestepContextualBernoulliBandits(
+    [Bandit(0.01), Bandit(0.02)],
+    [RandomWalkTrend(0.8, 1.2, 0.01), Periodicity(eg_weekly_periodicity)]
+)
