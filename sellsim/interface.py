09c4d581cc2466a32d5a51790d3000f2769f2971
ali-tny <aliteeney@googlemail.com>
2020-07-26 21:16:58 +1000

    Add Pseudo-static Bernoulli Bandits interface

--- sellsim/interface.py
+++ sellsim/interface.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, List, Tuple
+from typing import Callable, Dict, List
 
 import numpy as np
 
@@ -10,9 +10,9 @@ class Bandit():
         self.conversion_rate = 0
         pass
 
-    def action(self, context_multiplier: float = 1.0) -> bool:
+    def action(self, multiplier: float = 1.0) -> bool:
         """Use rand to see if success based on self.conversion_rate"""
-        conversion_rate = min(self.conversion_rate * context_multiplier, 1)
+        conversion_rate = min(self.conversion_rate * multiplier, 1)
         # TODO set random seeding etc
         return np.random.random() < conversion_rate
 
@@ -38,4 +38,24 @@ class HeirarchicalStaticBernoulliBandits():
         context_multiplier = 1.0
         for dimension, selected in selected_context.items():
             context_multiplier *= self.context[dimension][selected]
-        return self.bandits[selected_bandit].action(context_multiplier=context_multiplier)
+        return self.bandits[selected_bandit].action(multiplier=context_multiplier)
+
+
+class PseudoStaticBernoulliBandits():
+    def __init__(self, bandits: List[Bandit], periodicity: Callable[[int], float]):
+        self.bandits = bandits
+        self.periodicity = periodicity
+        self.timestep = 0
+
+    def step(self, selected_bandit: int) -> bool:
+        """Use selected_bandit to do bandit.action()"""
+        periodicity_multiplier = self.periodicity(self.timestep)
+        self.timestep += 1
+        return self.bandits[selected_bandit].action(multiplier=periodicity_multiplier)
+
+
+def eg_weekly_periodicity(timestep: int) -> float:
+    # Weekends are slightly better for conversions
+    weekly_periodicity = [1.0] * 5 + [1.1] * 2
+    day_of_week = timestep % 7
+    return float(weekly_periodicity[day_of_week])
