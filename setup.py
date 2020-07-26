5623817bfa05bb3da3f219af3c3df829c9fc4f17
dbcerigo <dbcerigo@gmail.com>
2020-07-26 11:32:23 +0100

    Project setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="sellsim",
+    packages=find_packages(),
+    version="0.0.a",
+    description="Simulating customer interactions with products, and bid auctions, at large scale.",
+    author="datavaluepeople",
+    license="",
+)
