e8a6fea5806d34e69873d480b1b524b91fccfce8
ali-tny <aliteeney@googlemail.com>
2020-07-26 21:02:38 +1000

    Flesh out basic interface and add heirarchical

--- sellsim/interface.py
+++ sellsim/interface.py
@@ -1,15 +1,20 @@
+from typing import Any, Dict, List, Tuple
 
+import numpy as np
 
 # basic multi armed bandit first
 
+
 class Bandit():
-
     def __init__(self, conversion_rate: float):
+        self.conversion_rate = 0
         pass
 
-    def action() -> bool:
+    def action(self, context_multiplier: float = 1.0) -> bool:
         """Use rand to see if success based on self.conversion_rate"""
-        pass
+        conversion_rate = min(self.conversion_rate * context_multiplier, 1)
+        # TODO set random seeding etc
+        return np.random.random() < conversion_rate
 
 
 class BasicDiscreteBernoulliBandits():
@@ -20,4 +25,17 @@ class BasicDiscreteBernoulliBandits():
 
     def step(self, selected_bandit: int) -> bool:
         """Use selected_bandit to do bandit.action()"""
-        pass
+        return self.bandits[selected_bandit].action()
+
+
+class HeirarchicalStaticBernoulliBandits():
+    def __init__(self, bandits: List[Bandit], context: Dict[str, Dict[str, float]]):
+        self.bandits = bandits
+        self.context = context
+
+    def step(self, selected_bandit: int, selected_context: Dict[str, str]) -> bool:
+        """Use selected_bandit to do bandit.action()"""
+        context_multiplier = 1.0
+        for dimension, selected in selected_context.items():
+            context_multiplier *= self.context[dimension][selected]
+        return self.bandits[selected_bandit].action(context_multiplier=context_multiplier)
