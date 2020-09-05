e4193e87f5fb68cd8d5d79f2df667ce0c060d80c
dbcerigo <dbcerigo@gmail.com>
2020-09-05 10:34:49 +0100

    Add period module
    All the name of this module should be reconsidered

---
+++ truman/period.py
@@ -0,0 +1,14 @@
+"""Module needs renaming as restricting and refactoring goes on.
+
+For now this contains envs which are interacting with cohorts of bandits in period blocks.
+
+
+cohort = 10k; dist1(strategy, t) for interaction; dist2(strategy, t) for conversion
+
+strat = {0, 1}
+
+cohort_strat_0 = [dist1(0, t) ]
+cohort_strat_1 = [dist1(1, t)  ]
+
+
+"""
