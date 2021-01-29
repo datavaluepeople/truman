15956d3066b68f7aa842cb2c011b051982d0e5e4
dbcerigo <dbcerigo@gmail.com>
2021-01-29 10:38:34 +0000

    Add agent info to history
    If fuckin works off the bat with the plot too ;)

--- tests/test_history.py
+++ tests/test_history.py
@@ -6,9 +6,9 @@ from truman import history
 @pytest.fixture
 def hist():
     hist = history.History()
-    hist.append(None, (10, 5), None, None, None)
-    hist.append(0, (10, 5), 5, False, {"info_1": 1, "info_2": 2})
-    hist.append(1, (10, 5), 5, True, {"info_1": 0, "info_2": 0})
+    hist.append(None, (10, 5), None, None, None, None)
+    hist.append(0, (10, 5), 5, False, {"info_1": 1, "info_2": 2}, {"agent_foo": 0})
+    hist.append(1, (10, 5), 5, True, {"info_1": 0, "info_2": 0}, {"agent_foo": 1})
     return hist
 
 
@@ -22,7 +22,8 @@ def test_observable(hist):
     _all = hist.all()
     assert _all[:4] == observables[:4]
     assert _all[4] == [None, {"info_1": 1, "info_2": 2}, {"info_1": 0, "info_2": 0}]
-    assert len(_all) == 5
+    assert _all[5] == [None, {"agent_foo": 0}, {"agent_foo": 1}]
+    assert len(_all) == 6
 
 
 def test_to_df(hist):
@@ -35,6 +36,7 @@ def test_to_df(hist):
         "done",
         "info_1",
         "info_2",
+        "agent_agent_foo",
     ]
     assert (df["action"].iloc[:1].isnull()).all()
     assert (df["action"].iloc[1:] == [0, 1]).all()
