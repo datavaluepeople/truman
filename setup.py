753c1c1ab25823ed69a2d56c4c453a42e69ef7d7
ali-tny <aliteeney@googlemail.com>
2021-05-15 16:25:14 +1000

    Add info to setup.py
    Specifically
    - the README contents for the long_description
    - the library URL (ie the github repo)
    - the library version as a constant
    - a stub for defining the requirements in the setup.py
    - an explicit requirement for python version (>=3.7)

--- setup.py
+++ setup.py
@@ -1,11 +1,26 @@
+import pathlib
+
 from setuptools import find_packages, setup
 
+VERSION = "0.0.1-alpha.1"
+
+REPO_ROOT = pathlib.Path(__file__).parent
+
+with open(REPO_ROOT / "README.md", encoding="utf-8") as f:
+    README = f.read()
+
+REQUIREMENTS = []
 
 setup(
     name="truman",
-    packages=find_packages(),
-    version="0.0.a",
+    version=VERSION,
     description="Simulating customer interactions with products and bid auctions at large scale.",
+    long_description=README,
+    long_description_content_type="text/markdown",
     author="datavaluepeople",
+    url="https://github.com/datavaluepeople/truman",
     license="MIT",
+    packages=find_packages(),
+    install_requires=REQUIREMENTS,
+    python_requires=">=3.7",
 )
